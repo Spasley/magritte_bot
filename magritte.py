e7dd416c24f154285d2f4aad84ca5f7154c5d62a
Spasley <proff.spasley@gmail.com>
2017-07-22 17:05:33 +0700

    fix indentation

--- magritte.py
+++ magritte.py
@@ -18,10 +18,10 @@ class BotHandler:
 
     def get_last_update(self):
         updates = self.get_updates()
-		if len(updates) > 0:
-		    last_update = updates[-1]
-		else:
-		    last_update = updates[len(updates)]
+        if len(updates) > 0:
+            last_update = updates[-1]
+        else:
+            last_update = updates[len(updates)]
         return last_update
 
     def send_message(self, chat_id, text):
